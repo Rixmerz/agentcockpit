7554af15e263350cba55a9e65770f3b8321fa4bb
Juan Pablo Díaz S. <juanpablo.diaz@cocha.com>
2025-12-27 02:18:30 -0300

    stable version V2 much funtionality core made it

--- one-term/src/App.jsx
+++ one-term/src/App.jsx
@@ -1,14 +1,22 @@
 import Terminal from "./components/Terminal";
+import ControlPanel from "./components/ControlPanel";
 import "./App.css";
 
 /**
  * Root App Component
- * Renders the Terminal application with integrated xterm
+ * Split layout: Terminal (left) + Control Panel (right)
  */
 function App() {
   return (
     <div className="app">
-      <Terminal />
+      <div className="split-container">
+        <div className="panel-left">
+          <Terminal />
+        </div>
+        <div className="panel-right">
+          <ControlPanel />
+        </div>
+      </div>
     </div>
   );
 }
